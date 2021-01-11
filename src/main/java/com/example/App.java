2119a5ef24354290c38c5303d38a02db2e5fed47
Azenga Kevin <azenga.kevin7@gmail.com>
2021-01-11 21:20:38 +0300

    Wip, hibernate first successfull test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,105 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import com.example.data.entities.Category;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class App {
 
+    static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("MangoesLeisureFacilityDb");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        createCategory("Jobless");
+        createCategory("Disabled");
+        createCategory("Student");
+
+        Optional<Category> category = readCategory(2);
+        System.out.print("Second Category: " + category);
+
+        List<Category> categories = readAllCategories();
+
+        System.out.println("\nAll Categories:");
+        categories.forEach(System.out::println);
+
+
+        ENTITY_MANAGER_FACTORY.close();
+    }
+
+    private static void createCategory(String name) {
+
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+
+        EntityTransaction entityTransaction = null;
+        try {
+
+            entityTransaction = entityManager.getTransaction();
+
+            entityTransaction.begin();
+
+            Category category = new Category(name);
+
+            entityManager.persist(category);
+
+            entityTransaction.commit();
+
+        } catch (Exception exception) {
+
+            if (entityTransaction != null) entityTransaction.rollback();
+
+            exception.printStackTrace();
+
+        } finally {
+            entityManager.close();
+        }
+
+    }
+
+    public static Optional<Category> readCategory(Integer id) {
+
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+
+        String query = "SELECT c FROM Category c WHERE c.id = :id";
+
+        TypedQuery<Category> typedQuery = entityManager.createQuery(query, Category.class);
+        typedQuery.setParameter("id", id);
+
+        try {
+            return Optional.of(typedQuery.getSingleResult());
+
+        } catch (Exception exception) {
+
+            exception.printStackTrace();
+
+        } finally {
+            entityManager.close();
+        }
+
+        return Optional.empty();
+    }
+
+    public static List<Category> readAllCategories() {
+
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+
+        String query = "SELECT  c FROM Category c";
+
+        TypedQuery<Category> typedQuery = entityManager.createQuery(query, Category.class);
+
+        try {
+            return typedQuery.getResultList();
+
+        } catch (Exception exception) {
+
+            exception.printStackTrace();
+
+        } finally {
+            entityManager.close();
+        }
+
+        return Collections.emptyList();
     }
 }
---
+++ src/main/java/com/example/data/entities/Category.java
@@ -0,0 +1,44 @@
+package com.example.data.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "categories")
+public class Category implements Serializable {
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Category(name=%s)", name);
+    }
+}
